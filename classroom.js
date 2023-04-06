dec8fb6eb201ec358df9a21f8e5271d18c5d463a
Dave89dc <127298096+Dave89dc@users.noreply.github.com>
2023-04-06 19:12:53 +0200

    Classroom-Manager

--- classroom.js
+++ classroom.js
@@ -5,15 +5,19 @@ class Classroom {
     };
 
     addStudent(student) {
-
+        if(!this.students.includes(student)) {
+            this.students.push(student);
+        };
     };
 
     removeStudent(student) {
-
+        if(this.students.includes(student)) {
+            this.students.splice(student, 1);
+        };
     };
 
     shuffleStudent() {
-
+        
     };
 
 };
--- main.js
+++ main.js
@@ -10,10 +10,15 @@ const student1 = new Student('Simone', 'Maccarone');
 const student2 = new Student('Luis Alberto', 'Castro');
 const student3 = new Student('Davide', 'Consigliere');
 const student4 = new Student('Francesco', 'Badile');
+const student5 = new Student('Pietro', 'Viglino');
+const student6 = new Student('Valentina', 'Cherubini');
+const student7 = new Student('Vlad', "P'yatnytskyy");
+const student8 = new Student('Daniele', 'Puggi');
+const student9 = new Student('Jing', 'Wong');
 
 const classroom1 = new Classroom([student1, student2, student3, student4]);
 
-function dislayClassroom(classroom) {
+function displayClassroom(classroom) {
 
 };
 
